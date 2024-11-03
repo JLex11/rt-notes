86a2b22428ad09af1c6e9d859b9e9c2a3ea32809
Lexx <chikyelfurioso2011@gmail.com>
2024-11-03 14:49:52 -0500

    refactor: Rename NotesTypeFilter component to NotesTypeFilters

--- src/components/NotesTypeFilters.tsx
+++ src/components/NotesTypeFilters.tsx
@@ -1,14 +1,15 @@
 import { useStore } from "@nanostores/preact"
+import { useMemo } from "preact/hooks"
 import { NOTES_TYPES } from "../consts"
 import { notes as notesStore } from "../store/notes"
 import { noteFilters, setType } from "../store/notesFilters"
 import cn from "../utils/cn"
 
-export default function NotesTypeFilter() {
+export default function NotesTypeFilters() {
   const notes = useStore(notesStore)
   const notesFilters = useStore(noteFilters)
 
-  const filteredNotes = notes.filter(note => {
+  const filteredNotes = useMemo(() => notes.filter(note => {
     const search = notesFilters.search.toLowerCase()
     const date = notesFilters.date
 
@@ -16,32 +17,38 @@ export default function NotesTypeFilter() {
       note.title.toLowerCase().includes(search) &&
       (date === null || note.date === date)
     )
-  })
+  }), [notes, notesFilters])
 
-  const filterIsActived = (type: string | null) => {
+  const isActive = (type: string | null) => {
     return notesFilters.type === (type as keyof typeof NOTES_TYPES)
   }
 
   return (
-    <ul class='flex gap-3 items-center p-1'>
+    <>
       <li
-        class={cn('border border-gray-300 px-3 py-1 rounded-xl hover:shadow hover:[-webkit-text-stroke-width:0.2px] transition-all', filterIsActived(null) ? 'bg-black text-white' : 'bg-gray-100 text-black')}
+        class={cn(
+          'border border-gray-300 px-3 py-1 rounded-xl hover:shadow hover:[-webkit-text-stroke-width:0.2px] transition-all flex-nowrap',
+          isActive(null) ? 'bg-black text-white' : 'bg-gray-100 text-black'
+        )}
         onClick={() => setType(null)}
       >
-        <button type="button" class='bg-transparent border-none'>Todas ({filteredNotes.length})</button>
+        <button type="button" class='bg-transparent border-none text-nowrap'>Todas ({filteredNotes.length})</button>
       </li>
       {
         Object.entries(NOTES_TYPES).map(([key, value]) => (
           <li
-            class={cn('border border-gray-300 px-3 py-1 rounded-xl hover:shadow hover:[-webkit-text-stroke-width:0.2px] transition-all', filterIsActived(key) ? value.activeStyle : value.color)}
+            class={cn(
+              'border border-gray-300 px-3 py-1 rounded-xl hover:shadow hover:[-webkit-text-stroke-width:0.2px] transition-all flex-nowrap',
+              isActive(key) ? value.activeStyle : value.color
+            )}
             onClick={() => setType(key as keyof typeof NOTES_TYPES)}
           >
-            <button type="button" class='bg-transparent border-none'>
+            <button type="button" class='bg-transparent border-none text-nowrap'>
               {value.label} ({filteredNotes.filter(note => note.type === key).length})
             </button>
           </li>
         ))
       }
-    </ul>
+    </>
   )
 }
