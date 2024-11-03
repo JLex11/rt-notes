5156048862c03ce7d3b171d64b782cb220af399e
Lexx <chikyelfurioso2011@gmail.com>
2024-11-03 14:51:31 -0500

    refactor: Update activeStyle in consts.ts for better hover effect

--- src/consts.ts
+++ src/consts.ts
@@ -2,21 +2,21 @@ export const NOTES_TYPES = {
   important: {
     label: 'Importantes',
     color: 'bg-red-100',
-    activeStyle: 'bg-red-200 border-red-400 [-webkit-text-stroke-width:1px] text-red-950',
+    activeStyle: 'bg-red-200 border-red-400 [-webkit-text-stroke-width:1px] hover:[-webkit-text-stroke-width:1px] text-red-950',
   },
   reminder: {
     label: 'Recordatorios',
     color: 'bg-green-100',
-    activeStyle: 'bg-green-200 border-green-400 [-webkit-text-stroke-width:1px] text-green-950',
+    activeStyle: 'bg-green-200 border-green-400 [-webkit-text-stroke-width:1px] hover:[-webkit-text-stroke-width:1px] text-green-950',
   },
   todo: {
     label: 'Tareas',
     color: 'bg-blue-200',
-    activeStyle: 'bg-blue-300 border-blue-400 [-webkit-text-stroke-width:1px] text-blue-950',
+    activeStyle: 'bg-blue-300 border-blue-400 [-webkit-text-stroke-width:1px] hover:[-webkit-text-stroke-width:1px] text-blue-950',
   },
   others: {
     label: 'Otros',
     color: 'bg-gray-100',
-    activeStyle: 'bg-gray-200 border-gray-400 [-webkit-text-stroke-width:1px] text-gray-950',
+    activeStyle: 'bg-gray-200 border-gray-400 [-webkit-text-stroke-width:1px] hover:[-webkit-text-stroke-width:1px] text-gray-950',
   },
 }
