1b6d715ec4e806b673a3e9d425ff7914af3c2e19
Lexx <93395794+JLex11@users.noreply.github.com>
2025-06-10 21:30:47 -0500

    refactor: cambiar el estado inicial del menú de comandos a abierto y mejorar la traducción de mensajes

--- src/components/CommandMenu.tsx
+++ src/components/CommandMenu.tsx
@@ -10,7 +10,7 @@ import {
 import { useEffect, useState } from 'react'
 
 export default function CommandMenu() {
-	const [open, setOpen] = useState(false)
+	const [open, setOpen] = useState(true)
 
 	useEffect(() => {
 		const down = (e: Event) => {
@@ -26,19 +26,27 @@ export default function CommandMenu() {
 	}, [])
 
 	return (
-		<CommandDialog open={open} onOpenChange={setOpen} label='Global Command Menu'>
-			<CommandInput />
+		<CommandDialog
+			open={open}
+			onOpenChange={setOpen}
+			label='Global Command Menu'
+			className='fixed top-[25%] left-[50%] translate-x-[-50%] translate-y-[-25%] m-auto z-50 bg-white/80 backdrop-blur-sm rounded-xl shadow-lg p-4 w-2xl flex flex-col'
+		>
+			<CommandInput
+				className='w-full p-3 focus-within:outline-2 focus-within:outline-blue-400 rounded-lg bg-white/20 transition-all outline-transparent shadow-2xl'
+				placeholder='Anything...'
+			/>
 			<CommandList>
-				<CommandEmpty>No results found.</CommandEmpty>
+				<CommandEmpty>No se obtuvieron resultados...</CommandEmpty>
 
-				<CommandGroup heading='Letters'>
+				<CommandGroup heading='Letters' className='rounded-lg p-3 mt-4 bg-white shadow-inner border border-gray-100'>
 					<CommandItem>a</CommandItem>
 					<CommandItem>b</CommandItem>
 					<CommandSeparator />
 					<CommandItem>c</CommandItem>
 				</CommandGroup>
 
-				<CommandItem>Apple</CommandItem>
+				<CommandItem className='rounded-lg p-3 mt-4 bg-white shadow-inner'>Apple</CommandItem>
 			</CommandList>
 		</CommandDialog>
 	)
