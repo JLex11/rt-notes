3c5c41a48031106d6ff70030cfdb528a969947df
Lexx <93395794+JLex11@users.noreply.github.com>
2025-06-10 19:44:19 -0500

    agregar manejo de activeFilters en los métodos de configuración de filtros

--- src/store/notesFilters.ts
+++ src/store/notesFilters.ts
@@ -5,26 +5,28 @@ interface NoteFilters {
 	search: string
 	type: keyof typeof NOTES_TYPES | null
 	date: Date | null
+	activeFilters: boolean
 }
 
 export const noteFilters = atom<NoteFilters>({
 	search: '',
 	type: null,
 	date: null,
+	activeFilters: false,
 })
 
 export const setSearch = (search: string) => {
-	noteFilters.set({ ...noteFilters.get(), search })
+	noteFilters.set({ ...noteFilters.get(), search, activeFilters: search.length > 0 })
 }
 
 export const setType = (type: NoteFilters['type']) => {
-	noteFilters.set({ ...noteFilters.get(), type })
+	noteFilters.set({ ...noteFilters.get(), type, activeFilters: Boolean(type) && type !== 'todo' })
 }
 
 export const setDate = (date: Date) => {
-	noteFilters.set({ ...noteFilters.get(), date })
+	noteFilters.set({ ...noteFilters.get(), date, activeFilters: Boolean(date) })
 }
 
 export const resetFilters = () => {
-	noteFilters.set({ search: '', type: null, date: null })
+	noteFilters.set({ search: '', type: null, date: null, activeFilters: false })
 }
