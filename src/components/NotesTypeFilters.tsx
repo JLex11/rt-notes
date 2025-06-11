115365e48e8526252af5b0eb83f13f8bb0851bf2
Lexx <93395794+JLex11@users.noreply.github.com>
2025-06-10 19:44:55 -0500

    refactor: actualizar importaciones y mejorar el manejo de texto en NotesTypeFilters

--- src/components/NotesTypeFilters.tsx
+++ src/components/NotesTypeFilters.tsx
@@ -1,9 +1,10 @@
-import { useStore } from '@nanostores/preact'
-import { useMemo } from 'preact/hooks'
+import { useStore } from '@nanostores/react'
+import { useMemo } from 'react'
 import { NOTES_TYPES } from '../consts'
 import { notes as notesStore } from '../store/notes'
 import { noteFilters, setType } from '../store/notesFilters'
 import cn from '../utils/cn'
+import { normalizeText } from '../utils/normalizeText'
 
 export default function NotesTypeFilters() {
 	const notes = useStore(notesStore)
@@ -12,10 +13,10 @@ export default function NotesTypeFilters() {
 	const filteredNotes = useMemo(
 		() =>
 			notes.filter(note => {
-				const search = notesFilters.search.toLowerCase()
+				const search = normalizeText(notesFilters.search.toLowerCase())
 				const date = notesFilters.date
 
-				return note.title.toLowerCase().includes(search) && (date === null || note.date === date)
+				return normalizeText(note.title.toLowerCase()).includes(search) && (date === null || note.date === date)
 			}),
 		[notes, notesFilters]
 	)
@@ -27,28 +28,28 @@ export default function NotesTypeFilters() {
 	return (
 		<>
 			<li
-				class={cn(
+				className={cn(
 					'border border-gray-300 px-3 py-1 rounded-xl hover:shadow hover:[-webkit-text-stroke-width:0.2px] transition-all flex-nowrap',
 					isActive(null) ? 'bg-black text-white' : 'bg-gray-100 text-black'
 				)}
 				onClick={() => setType(null)}
 				onKeyDown={e => e.key === 'Enter' && setType(null)}
 			>
-				<button type='button' class='bg-transparent border-none text-nowrap'>
+				<button type='button' className='bg-transparent border-none text-nowrap'>
 					Todas ({filteredNotes.length})
 				</button>
 			</li>
 			{Object.entries(NOTES_TYPES).map(([key, value]) => (
 				<li
 					key={key}
-					class={cn(
-						'border border-gray-300 px-3 py-1 rounded-xl hover:shadow hover:[-webkit-text-stroke-width:0.2px] transition-all flex-nowrap',
+					className={cn(
+						'px-3 py-1 rounded-xl hover:[-webkit-text-stroke-width:0.2px] transition-all flex-nowrap',
 						isActive(key) ? value.activeStyle : value.color
 					)}
 					onClick={() => setType(key as keyof typeof NOTES_TYPES)}
 					onKeyDown={e => e.key === 'Enter' && setType(key as keyof typeof NOTES_TYPES)}
 				>
-					<button type='button' class='bg-transparent border-none text-nowrap'>
+					<button type='button' className='bg-transparent border-none text-nowrap'>
 						{value.label} ({filteredNotes.filter(note => note.type === key).length})
 					</button>
 				</li>
